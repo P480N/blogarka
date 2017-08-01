fb94294c8c52120ee2067a90153eebe498914fd4
p480n <p480nzt@gmail.com>
2017-07-31 19:29:16 -0500

    otro 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
 
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
   ⁠⁠⁠⁠⁠config.assets.compile = true
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
